a4280c9f8b6a36a62a1ab033296bb08bede5b2a0
lo duy hoan <duyhoan3721@gmail.com>
2023-10-24 20:18:54 +0700

    controller

--- app/Http/Controllers/QuesionController.php
+++ app/Http/Controllers/QuesionController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quesion;
 use Illuminate\Http\Request;
 
 class QuesionController extends Controller
 {
-    //
+    public function findId($id)
+    {
+        return Quesion::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $createData = $request->all();
+
+        if (Quesion::create($createData)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function save(Request $request, int $id)
+    {
+        $saveData = $request->all();
+        $quesion = $this->findId($id);
+        if ($quesion->update($saveData)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function destroy($id)
+    {
+        if (Quesion::destroy($id)) {
+            return true;
+        }
+        return false;
+    }
 }
--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quiz;
 use Illuminate\Http\Request;
 
 class QuizController extends Controller
 {
-    //
+    public function getAll()
+    {
+        return Quiz::query()->with('quesion','quesion.answer');
+    }
+
+    public function findId($id)
+    {
+        return Quiz::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $createData = $request->all();
+
+        if (Quiz::create($createData)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function save(Request $request, int $id)
+    {
+        $saveData = $request->all();
+        $quiz = $this->findId($id);
+        if ($quiz->update($saveData)) {
+            return true;
+        }
+        return false;
+    }
+
+    public function destroy($id)
+    {
+        if (Quiz::destroy($id)) {
+            return true;
+        }
+        return false;
+    }
 }
